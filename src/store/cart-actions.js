6e5cae5fdc529712c2ad61cf0ce29d596cef9a81
HamidrezaBahmani <hb.hamidrezabahmani.com@gmail.com>
2023-12-02 23:16:15 +0330

    Finalizing the Fetching Logic

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -19,7 +19,12 @@ export const fetchCartData = () => {
 
     try {
       const cartData = await fetchData();
-      dispatch(cartActions.replaceCart(cartData));
+      dispatch(
+        cartActions.replaceCart({
+          items: cartData.items || [],
+          totalQuantity: cartData.totalQuantity,
+        })
+      );
     } catch (error) {
       dispatch(
         uiActions.showNotification({
@@ -46,7 +51,10 @@ export const sendCartData = (cart) => {
         "https://http-react-a0ff8-default-rtdb.firebaseio.com/cart.json",
         {
           method: "PUT",
-          body: JSON.stringify(cart),
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+          }),
         }
       );
       if (!response.ok) {
